1485c167e6803cf2ff6d735371180e62c2295c5a
MrJake222 <norbertzpilicy@gmail.com>
2023-03-27 13:05:11 +0200

    random line generation 2

--- src/Line.py
+++ src/Line.py
@@ -1,4 +1,9 @@
 class Line:
     def __init__(self, stops: list[int], graph):
         self.stops = stops # ordered list of stops
-        self.edges = [(v, u) for v in stops for u in graph[v] if (v, u) in graph.edges]
+        # self.edges = [(v, u) for v in stops for u in graph[v] if (v, u) in graph.edges]
+        self.edges = []
+        for i in range(len(self.stops) - 1):
+            v = self.stops[i]
+            u = self.stops[i+1]
+            self.edges.append((v, u))
--- src/line_generation.py
+++ src/line_generation.py
@@ -13,10 +13,16 @@ def gen_random_line(length):
     # random.shuffle(V)
     line = Line(V, graph)
     print(line.stops)
-    print(line.edges)
+    #print(line.edges)
+    for u, v in line.edges:
+        print(u, v)
+        graph.add_edge(u, v, color='r', weight=3)
 
 
-gen_random_line(3)
+gen_random_line(7)
 
-# nx.drawing.draw_networkx(graph)
-# plt.show()
+colors = [graph[u][v]['color'] if 'color' in graph[u][v] else "black" for u,v in graph.edges()]
+weights = [graph[u][v]['weight'] if 'weight' in graph[u][v] else 1 for u,v in graph.edges()]
+
+nx.draw(graph, with_labels=True, edge_color=colors, width=weights)
+plt.show()
